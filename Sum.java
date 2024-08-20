ea8e442e9fb70b0673b56f8adf6267ba08418526
Onkar Kulkarni <onkarskulkarni09.58@gmail.com>
2024-08-21 01:36:56 +0530

    Added constructor

--- Sum.java
+++ Sum.java
@@ -3,13 +3,22 @@ public class Sum {
     int age;
     String name;
 
+    public Sum() {
+        this.age = 20;
+        this.name = "onkar";
+    }
+
     public static void main(String[] args) {
+
+        Sum sum = new Sum();
+
+        System.out.println("printing age of SUM :-> " + sum.age);
 
         int a1 = 10, b = 20;
 
-        int sum = a1 + b;
+        int addition = a1 + b;
 
-        System.out.println("Sum is :-> " + sum);
+        System.out.println("Sum is :-> " + addition);
 
         if (a1 > b) {
             System.out.println("A is greater than B");
