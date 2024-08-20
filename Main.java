9fab32ef60d1164bb80a124b56b907301713d6d9
Onkar Kulkarni <onkarskulkarni09.58@gmail.com>
2024-08-21 02:33:07 +0530

    Newly added features in Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        String str = "Onkar";
-        String str2 = "Kulkarni";
+        String str1 = "Ram";
+        String str2 = "Sham";
 
-        System.out.println("Onakr Kulkarni..!!!");
+        Integer num = 10;
+
+        System.out.println("Newly created Integer :->  " + num);
+
+        System.out.println("Hello Onkar New Branch..!!!");
+
+        System.out.println("Onkar Sudhir Kulkarni..!!!");
+
+        System.out.println("Printing Str1 & Str2 :-> " + str1 + "  " + str2);
 
     }
 
