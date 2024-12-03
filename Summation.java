62d3ad6c78055dc63cc9dda82337d8fd5482e289
Onkar Kulkarni <onkarskulkarni09.58@gmail.com>
2024-12-04 00:42:20 +0530

    Added 4 & 5 numbers

--- Summation.java
+++ Summation.java
@@ -9,5 +9,15 @@ public class Summation {
         sum = a + b + c;
         System.out.println("After Adding 3 numbers :-> " + sum);
 
+        // Adding 4th numbers
+        int d = 50;
+        sum = a + b + c + d;
+        System.out.println("After Adding 4 numbers :-> " + sum);
+
+        // Adding 5 numbers
+        int e = 50;
+        sum = a + b + c + d + e;
+        System.out.println("After Adding 5 numbers :-> " + sum);
+
     }
 }
