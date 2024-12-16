a9adddf58cb4260a5c782c4ceb6ea3b702eee7a9
Onkar Kulkarni <onkarskulkarni09.58@gmail.com>
2024-12-16 18:40:53 +0530

    branch named changed

--- Index.java
+++ Index.java
@@ -3,11 +3,12 @@ public class Index {
 
         System.out.println("Hello what are you doing");
 
-        int a=10, b=20, mul=0;
-        mul = a*b;
+        int a = 10, b = 20, mul = 0;
+        mul = a * b;
 
-        System.out.println("Multiplication of 2 numbers is :-> "+mul );
+        System.out.println("Multiplication of 2 numbers is :-> " + mul);
 
+        System.out.println("Changed branch name");
 
     }
 }
